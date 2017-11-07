33b3f89251437f66dc5c3ad60f9263a48599d043
Amirali Null <amiralinull@gmail.com>
2017-11-07 20:28:21 +0330

    upto 0.2.1, more in README

--- irkv.gemspec
+++ irkv.gemspec
@@ -1,13 +1,13 @@
 Gem::Specification.new do |s|
     s.name        = 'irkv'
-    s.version     = '0.0.1'
-    s.date        = '2017-04-02'
+    s.version     = '0.2.1'
+    s.date        = '2017-04-03'
     s.summary     = ''
     s.description = "A simple key value store"
     s.authors     = ["Amirali Esfandiari"]
     s.email       = 'amiralinull@gmail.com'
     s.files       = ["lib/irkv.rb"]
     s.homepage    =
-      'http://gitlab.org/amiralinull/irkv/'
+      'http://gitlab.com/amiralinull/irkv/'
     s.license       = 'GPL-3.0+'
   end
--- lib/irkv.rb
+++ lib/irkv.rb
@@ -3,14 +3,20 @@ require 'json'
 class IRKV
     def initialize(file)
         @file = file
+
+        # Check file's not exist and false create it
+        if !File.exist?(@file)
+          File.new @file
+        end
+
         return true
     end
 
     def set(key, value)
         f = File.open(@file, 'r')
         parsed = JSON f.read
-        parsed[key] = value
         f.close
+        parsed[key] = value
         f = File.open(@file, 'w')
         f.write(JSON.generate parsed)
         f.close
@@ -22,5 +28,27 @@ class IRKV
         parsed = JSON f.read
         f.close
         return parsed[key]
+    end
+
+    def delete(key)
+        f = File.open(@file, 'r')
+        parsed = JSON f.read
+        f.close
+        parsed.delete(key)
+        f = File.open(@file, 'w')
+        f.write(JSON.generate parsed)
+        f.close
+        return true
+    end
+
+    def update(key, value)
+        f = File.open(@file, 'r')
+        parsed = JSON f.read
+        f.close
+        parsed[key] = value
+        f = File.open(@file, 'w')
+        f.write(JSON.generate parsed)
+        f.close
+        return true
     end
 end
